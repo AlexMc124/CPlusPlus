8f5d0f02bad2a9559699da14b66af2c2bb267d49
AlexMc124 <alex.mcintosh124@gmail.com>
2021-08-24 18:33:49 +0100

    working with strings in c++

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,14 @@ int main()
 	std::cout << "He was " << age << " years old" << std::endl;
 	std::cout << "He liked the name " << characterName << std::endl;
 	std::cout << "He did not like being " << age << std::endl;
+	// len
+	std::cout << characterName.length() << std::endl;
+	//char_at
+	std::cout << characterName[2] << std::endl;
+	// Will tell me where the string occurs
+	std::cout << characterName.find("h", 0) << std::endl;
+	// substr
+	std::cout << characterName.substr(1, 2) << std::endl;
 
 	return 0;
 }
