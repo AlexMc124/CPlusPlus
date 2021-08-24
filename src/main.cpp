2beb9fcb063bfb23aae504cf1f788863458747c1
AlexMc124 <alex.mcintosh124@gmail.com>
2021-08-24 18:28:50 +0100

    wrote a short story to demonstrate variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
+int main()
 {
-	std::cout << "    /|" << std::endl;
-	std::cout << "   / |" << std::endl;
-	std::cout << "  /  |" << std::endl;
-	std::cout << " /   |" << std::endl;
-	std::cout << "/____|" << std::endl;
+	std::string characterName = "John";
+	int age = 70;
+	std::cout << "There once was a man named " << characterName << std::endl;
+	std::cout << "He was " << age << " years old" << std::endl;
+	std::cout << "He liked the name " << characterName << std::endl;
+	std::cout << "He did not like being " << age << std::endl;
 
 	return 0;
 }
